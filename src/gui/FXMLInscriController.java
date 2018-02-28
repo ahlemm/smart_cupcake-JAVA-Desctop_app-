a20ce7f1803d99b68f74a314843821754cffb0ff
ahlem_midini <ahlem.midini@esprit.tn>
2018-02-28 17:50:58 +0100

    First commit

--- src/gui/FXMLInscriController.java
+++ src/gui/FXMLInscriController.java
@@ -7,6 +7,7 @@ package gui;
 import entite.Formation;
 import java.util.*;
 import entite.Inscription;
+import java.io.IOException;
 import service.ServiceFormation;
 import service.ServiceInscription;
 import java.net.URL;
@@ -24,7 +25,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -36,6 +41,7 @@ import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import javafx.util.Callback;
 
 /**
@@ -98,6 +104,8 @@ public class FXMLInscriController implements Initializable {
     ServiceInscription serviceinscription = new ServiceInscription();
     @FXML
     private TextField form;
+    @FXML
+    private Button retourinscri2;
     
     private List<Formation> findFormation() throws SQLException {
         return ServiceFormation.afficher();
@@ -201,7 +209,17 @@ debud.setText(String.valueOf(newValue.getDate_debut()));
            catch (SQLException ex) {
                 Logger.getLogger(FXMLInscriController.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } }
+        }
+
+    @FXML
+    private void retourinscri2(ActionEvent event) throws IOException {  FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuClient.fxml"));
+            Parent root = loader.load();
+           Scene homePageScene=new Scene(root); 
+           Stage appStage =(Stage) ((Node) event.getSource()).getScene().getWindow(); 
+           appStage.setScene(homePageScene); 
+           appStage.show();
+    }
+}
         
      
         
